8d4e7b064c30e7df733b16da364884798cd07f09
vbaghdas <vbaghdas@gmail.com>
2017-08-09 15:06:19 -0700

    Skeleton Made

---
+++ dom.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+//Play Field
+
+
+for(var i = 0; i < 64; i++){
+        var $div1 = $('<div>', {
+            class: "tile",
+            html: i
+        });
+        $div1.appendTo('.play_field');
+}
+
+
+//Left Field
+
+
+//Bottom Field
+
+
+});
