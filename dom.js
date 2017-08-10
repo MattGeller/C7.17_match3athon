d041d4fdac8fdfdd5eb423f263ecfd0ea346097c
vbaghdas <vbaghdas@gmail.com>
2017-08-10 11:44:20 -0700

    Animation

--- dom.js
+++ dom.js
@@ -1,46 +1,132 @@
 $(document).ready(function () {
 
-//Header
+
+// Header
+
 var $header = $('<header>');
 $header.appendTo('body');
 
+    var $h1 = $('<h1>', {
+        text: "SPACE WARS SAGA"
+    });
+    $h1.appendTo('header');
 
-//Game Area
+
+// Game Area
+
 var $div1 = $('<div>', {
-    id: "game_area",
+    id: 'game_area',
 });
 $div1.appendTo('body');
 
-//Left Field
+
+// Left Field
+
 var $div2 = $('<div>', {
-    class: "left_field",
+    class: 'left_field'
 });
 $div2.appendTo('#game_area');
 
-//Play Field
+    var $h2 = $('<h2>', {
+        text: "SCORE"
+    });
+    $h2.appendTo('.left_field');
 
-var $div3 = $('<div>', {
-    class: "play_field",
+
+    // Progress Bar
+
+    var $div3 = $('<div>', {
+        class: 'progress progressbar',
+        id: 'score'
+    });
+    $div3.appendTo('.left_field');
+
+
+// Display Stats
+
+    function display_stats(){
+        $('#score > .value').text(score);
+
+        $(function() {
+            $("#score").progressbar({
+                value: score
+            });
+        } );
+    }
+
+
+// Play Field
+
+var $div4 = $('<div>', {
+    class: 'play_field'
 });
-$div3.appendTo('#game_area');
+$div4.appendTo('#game_area');
 
-for(var i = 0; i < 64; i++){
-        var $div4 = $('<div>', {
-            class: "tile",
-            html: i
+    for(var i = 0; i < 64; i++){
+        var $div5 = $('<div>', {
+            class: 'tile',
+            id: i
         });
-        $div4.appendTo('.play_field');
-}
+        $div5.css('order', i);
+        $div5.appendTo('.play_field');
+    }
 
-//Right Field
-var $div5 = $('<div>', {
-    class: "right_field",
+    $('.tile').click(function(){
+        $(this).css('order', 9);
+        $(this).css({bottom: '86px'});
+        $('#9').remove();
+        var $div7 = $('<div>', {
+            class: 'tile',
+            id: 1
+        });
+        $div7.css('order', 1);
+        $div7.html('<img src="assets/mars.png">');
+        $div7.appendTo('.play_field');
+        $(this).animate({top: '0'});
+    });
+
+
+
+// Right Field
+
+var $div6 = $('<div>', {
+    class: 'right_field'
 });
-$div5.appendTo('#game_area');
+$div6.appendTo('#game_area');
+
+    $h2 = $('<h2>', {
+        text: "LEVEL"
+    });
+    $h2.appendTo('.right_field');
+
+    $p = $('<p>', {
+        class: 'level',
+        text: "3"
+    });
+    $p.appendTo('.right_field');
 
 
-//Footer
+// Footer
+
 var $footer = $('<footer>');
 $footer.appendTo('body');
+
+
+// Win Modal
+
+// var $div6 = $('<div>',{
+//    class: 'modal',
+//     id: 'win_modal',
+//     role: 'dialog'
+// });
+// $div6.appendTo('body');
+//
+//     var $div7 = $('<div>',{
+//        class: 'modal-dialog win'
+//     });
+//     $div7.appendTo.($div6);
+
+
+
 
 });
